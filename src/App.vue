55a00557c18a7004cdb9044311264c0d0a769d3b
poemsforaphrodite <pushpendersolanki_co21a6_23@dtu.ac.in>
2023-12-30 02:21:06 +0530

    Refactor code for improved performance

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <ul>
+    <li><router-link to="/">Home</router-link> |</li>
+    <li><router-link to="/about">About</router-link></li>
+  </ul>
+  <router-view />
+</template>
+<script setup>
+import {RouterView, RouterLink} from 'vue-router'
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1> HOasdasdME</h1>
+    <CounterButton />
+</template>
+
+<script setup>
+import CounterButton from '../components/CounterButton.vue';
+</script>
